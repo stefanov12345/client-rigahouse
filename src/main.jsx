f01afe647cc124003755536b3f9918ea77e4e45c
stefano <stefano_vannucci@hotmail.com>
2024-05-26 16:26:26 +0200

    test2

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       domain='dev-qnwjq68ysys1hiri.us.auth0.com'
       clientId='MhxtqFZejJUM7gB2jvfEFQ4dE8goUvtU'
       authorizationParams={{
-        redirect_uri: "http://localhost:5173/",
+        redirect_uri: "https://backend-rigahouse-gl0jxrqug-stefanos-projects-e0d54527.vercel.app/",
       }}
       audience='https://backend-rigahouse-gl0jxrqug-stefanos-projects-e0d54527.vercel.app/'
       scope='opneid profile email'
