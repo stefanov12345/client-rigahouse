3f76a0d2eded443dc492dd19959af3a4dbd78de4
stefano <stefano_vannucci@hotmail.com>
2024-05-31 02:28:13 +0200

    churrasos de regalo para todos 4

--- src/main.jsx
+++ src/main.jsx
@@ -15,15 +15,15 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         redirect_uri:
           process.env.NODE_ENV === "development"
             ? "http://localhost:5173"
-            : "https://https://client-rigahouse.vercel.app/",
+            : "https://https://client-rigahouse.vercel.app",
       }}
       // audience='https://backend-rigahouse-gl0jxrqug-stefanos-projects-e0d54527.vercel.app/'
       audience={
         process.env.NODE_ENV === "development"
           ? "http://localhost:5173"
-          : "https://https://client-rigahouse.vercel.app/"
+          : "https://https://client-rigahouse.vercel.app"
       }
-      scope='opneid profile email'
+      scope='openid profile email'
     >
       <MantineProvider>
         <App />
